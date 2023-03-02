3971fb265016086a91af367452333726dc82543d
[Mykhalchan] <[d.mikhalchan@gmail.com]>
2023-03-02 16:43:10 +0100

    Good

--- src/Main/Java/com/hillel/mykhalchan/lessons/lesson9/Homework_12.java
+++ src/Main/Java/com/hillel/mykhalchan/lessons/lesson9/Homework_12.java
@@ -1,8 +1,4 @@
 package com.hillel.mykhalchan.lessons.lesson9;
-import java.util.Arrays;
-import java.util.Scanner;
-
-public class Homework_12 {
     class Person {
 
         public Person(String name, String surname, String city, String phone) {
@@ -21,7 +17,7 @@ public class Homework_12 {
 
         public String personInfo() {
 
-            return "Позвонить гражданину " + name + " " + surname + " из города " + city + " можно по номеру " + phone;
+            return "Позвонить гражданину " + name + " " + surname + " из города " + city + " можно по телефону " + phone;
 
         }
 
@@ -37,13 +33,13 @@ public class Homework_12 {
 
     class Main {
 
-        public void main(String[] args) {
+        public static void main(String[] args) {
 
-            Person person1 = new Person("Bob", "Chep", "Нью-Йорк", "2936729462846");
+            Person person1 = new Person("Сергей", "Михайлов", "Берлин", "+38 097 777 77 77");
 
-            Person person2 = new Person("Janie", "Wook", "Шанхай", "12325436454");
+            Person person2 = new Person("Владимир", "Снигуренко", "Лондон", "+38 063 777 77 77");
 
-            Person person3 = new Person("Sheron", "Raze", "London", "37754634686");
+            Person person3 = new Person("Николай", "Дмитриев", "Нью-Йорк", "+38 050 777 77 77");
 
             System.out.println(person1.personInfo());
 
@@ -54,4 +50,3 @@ public class Homework_12 {
         }
 
     }
-}
